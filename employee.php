4b13e1b73ba24d37a0fd3e3899357c268a6d3489
Mozammel Haque <mzmlhaque61@gmail.com>
2015-11-29 17:50:19 +0600

    added employe page

---
+++ employee.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mozammel
+ * Date: 11/23/2015
+ * Time: 2:35 AM
+ */
+
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Welcome Employee Name</h3>
+            <h4>Please select state</h4>
+
+            <form action="" role="form">
+                <div class="form-group">
+                    <select name="employe-action" id="">
+                        <option selected>--Please Select--</option>
+                        <option value="time_in">Time In</option>
+                        <option value="time_out">Time Out</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- End row -->
+</div>
+<!-- End container -->
